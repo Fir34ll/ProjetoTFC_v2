7646bc71bd507633889a2f2be16faa8746ccc2de
Rafael Ramos <rafaelfilhoramos@hotmail.com>
2024-10-01 18:57:32 -0300

    Att

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,28 +11,23 @@ def test_first_page(client):
     """Testa a rota '/'."""
     response = client.get('/')
     assert response.status_code == 200
-    assert b'first' in response.data  # Verifica se 'first' está presente no HTML
+    assert b'investidor' in response.data  # Verifica se 'investidor' está presente no HTML
 
-def test_iniciante_redirect(client):
+def test_grafico_redirect(client):
     """Testa a rota '/iniciante' para redirecionamento quando não logado."""
-    response = client.get('/iniciante')
+    response = client.get('/indexgrafico')
     assert response.status_code == 302  # Verifica redirecionamento
-    assert b'login' in response.headers['Location']  # Verifica se redireciona para a página de login
+    assert 'login' in response.headers['Location']  # Verifica se redireciona para a página de login
 
-def test_intermediario_redirect(client):
-    """Testa a rota '/intermediario' para redirecionamento quando não logado."""
-    response = client.get('/intermediario')
-    assert response.status_code == 302
-    assert b'login' in response.headers['Location']
 
-def test_avancado_redirect(client):
-    """Testa a rota '/avançado' para redirecionamento quando não logado."""
-    response = client.get('/avançado')
+def test_fundos_redirect(client):
+    response = client.get('/indexfundos')
     assert response.status_code == 302
-    assert b'login' in response.headers['Location']
+    assert 'login' in response.headers['Location']
+
 
 def test_indexcalculadora_redirect(client):
-    """Testa a rota '/indexcalculadora' para redirecionamento quando não logado."""
     response = client.get('/indexcalculadora')
     assert response.status_code == 302
-    assert b'login' in response.headers['Location']
+    assert 'login' in response.headers['Location']
+
